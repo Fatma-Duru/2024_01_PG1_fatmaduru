1d59da801a5851492f9a400fce5d269c65242662
Fatma-Duru <duruf814@gmail.com>
2024-02-15 04:35:39 +0300

    3. bölüm için deneme

--- src/chapter03/chapter03_01.java
+++ src/chapter03/chapter03_01.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class chapter03_01 {
     public static void main(String[] args){
         //3. Bölüm 1. Soru
+        Scanner scannr   = new Scanner(System.in);
+        System.out.print("Bir a değeri giriniz");
+        double a = scannr.nextDouble();
+        System.out.print("Bir b değeri giriniz");
+        double b = scannr.nextDouble();
+        System.out.print("Bir c değeri giriniz");
+        double c = scannr.nextDouble();
 
 
 
